e78926bf393c7d9558d3e055ea020d4e110cc797
Tassawar-Abbas <tas80066@gmail.com>
2024-09-01 14:14:31 +0500

    adding summary skills eductaion

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -15,23 +15,16 @@ const defaultOptions = {
 };
 
 const FeatureCard = ({
-  icon,
   title,
   degree,
   duration,
-  content1,
-  content2,
   index,
 }) => (
   <div
     className={`flex flex-row p-6 rounded-[20px]
 	${index === educationList.length - 1 ? "mb-0" : "mb-6"} feature-card`}
   >
-    <div
-      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
-    >
-      <img src={icon} alt="icon" className="w-[80%] h-[80%] object-contain" />
-    </div>
+ 
     <div className="flex-1 flex flex-col ml-4">
       <h4 className="font-poppins font-semibold text-white text-[20px] leading-[30px] mb-1 text-gradient">
         {title}
@@ -42,14 +35,7 @@ const FeatureCard = ({
       <p className="font-poppins font-normal text-dimWhite text-[14px] leading-[30px] mb-1">
         {duration}
       </p>
-      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[30px] mb-1">   
-      ● {content1}
-      </p>
-      {content2 && (
-        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[30px] mb-1">     
-      ● {content2}
-        </p>
-      )}
+      
     </div>
   </div>
 );
--- src/components/SkillsAndExperience.jsx
+++ src/components/SkillsAndExperience.jsx
@@ -63,11 +63,6 @@ const ExperienceCard = (props) => {
     transition={{ duration: 1 }}
     >
       <div className="flex flex-row items-center mb-6">
-        <img
-          src={props.logo}
-          alt={props.organisation}
-          className="w-[52px] h-[52px] rounded-full z-[2]"
-        />
         <h4 className="font-poppins font-semibold text-[20px] text-gradient leading-[32px] ml-2">
           {props.organisation}
         </h4>
@@ -80,15 +75,13 @@ const ExperienceCard = (props) => {
             } ml-4`}
           >
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+            <h3 className="text-lg font-semibold text-gray-700 dark:text-white">
               {position.title}
             </h3>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
               {position.duration}
             </time>
-            {position.content.map((info, index) => (
-              <Content index={index} {...info} />
-            ))}
+           
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"></p>
           </li>
         ))}
